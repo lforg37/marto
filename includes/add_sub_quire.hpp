645b49466881cb29761051e3b032917c81c8ac82
UGUEN Yohann <yohann.uguen@inria.fr>
2019-02-08 16:26:46 +0100

    PositHLS: add_sub_quire draft

--- includes/add_sub_quire.hpp
+++ includes/add_sub_quire.hpp
@@ -1,12 +1,29 @@
 #pragma once
 #include "ap_int.h"
-#include "posit_dim.tpp"
+#include "posit_dim.hpp"
 
-template<size_t N>
+
+template<int N>
 Quire<N> add_sub_quire(
 		Quire<N> quire, 
 		PositProd<N> input,
-	   	ap_uint<1> is_sub
+	   	ap_uint<1> isSub
 ){
-	return 0;
+	
+	ap_int<PositDim<N>::ProdSignificandSize> inputSignificand = input.getSignificand();
+	ap_int<PositDim<N>::ProdSignificandSize> complementedInputIfIsSub;
+	
+	#pragma HLS UNROLL
+	for (int i=0; i<N; i++){
+		complementedInputIfIsSub[i] = input.getBit(i) ^ isSub;
+	}
+
+	ap_int<PositDim<N>::ExtQuireSize-1> shiftedInput = complementedInputIfIsSub<<(input.getExp());
+	ap_uint<PositDim<N>::ExtQuireSize-1> quireWithoutSignAndNARBit = quire.getQuireWithoutNaR();
+
+	ap_uint<PositDim<N>::ExtQuireSize-1> sumResult = shiftedInput + quireWithoutSignAndNARBit + isSub;
+
+	ap_uint<1> resultIsNaR = quire.getIsNaR() || input.getIsNaR();
+
+	return Quire<N>(resultIsNaR.concat(sumResult));
 }
