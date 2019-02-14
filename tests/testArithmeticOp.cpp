5cdd4fe66b6fd1bd46567d473daa3bbc2c51b455
UGUEN Yohann <yohann.uguen@inria.fr>
2019-02-14 17:36:18 +0100

    PositHLS: Posit addition validated

--- tests/testArithmeticOp.cpp
+++ tests/testArithmeticOp.cpp
@@ -20,8 +20,9 @@ using namespace std;
 
 BOOST_AUTO_TEST_CASE(TestAllSumPosit8) 
 {
+
 	// ap_uint<16> value10 = 0b1111111111111111;
-	// ap_uint<16> value20 = 0b0000000000000000;
+	// ap_uint<16> value20 = 0b0000000000000001;
 	// PositValue<16> a = posit_decoder(value10);
 	// PositValue<16> b = posit_decoder(value20);
 	// // PositValue<16> b = posit_decoder((ap_uint<16>)0b0110111100000111);
@@ -50,17 +51,15 @@ BOOST_AUTO_TEST_CASE(TestAllSumPosit8)
 	// t.printContent();
 	// exit(0);
 
-	// uint16_t value1 = 0;
-	uint16_t value2 = 0;
 	uint64_t counter = 0;
 	uint64_t TOTAL_TESTS = (((uint64_t)1)<<32);
 	unsigned int error_counter = 0;
-	#pragma omp parallel for private(value2)
-	for(uint16_t value1 = 0; value1 <((1<<16)-1); value1++){
+	#pragma omp parallel for 
+	for(uint32_t value2 = 0; value2 < (1<<16); value2++){
 		auto value2Encoding = PositEncoding<16> (value2);
 		auto decoded2 = posit_decoder(value2Encoding);
 
-		do {
+		for(uint32_t value1 = 0; value1 < (1<<16); value1++){
 			auto value1Encoding = PositEncoding<16> (value1);
 			auto decoded1 = posit_decoder(value1Encoding);
 			auto sum = posit_add(decoded1, decoded2);
@@ -69,16 +68,7 @@ BOOST_AUTO_TEST_CASE(TestAllSumPosit8)
 			posit16_t positValue2 = castP16(value2);
 			posit16_t positSum = p16_add(positValue1, positValue2);
 			ap_uint<16> softpositSum = (ap_uint<16>) castUI(positSum);
-			// ap_uint<16> abs_diff;
-			// if(encoded>softpositSum){
-			// 	abs_diff = encoded-softpositSum;
-			// }
-			// else{
-			// 	abs_diff = softpositSum-encoded;
-			// }
 			if(!(encoded == softpositSum)){
-			// if(log2((double)abs_diff)>1){
-				// error_counter++;
 				fprintf(stderr, "\n\n\n\n");
 				fprintf(stderr, "=== Inputs === \n");
 				printApUint(value1Encoding);
@@ -92,16 +82,14 @@ BOOST_AUTO_TEST_CASE(TestAllSumPosit8)
 
 				BOOST_REQUIRE_MESSAGE(false, "Sum of " << value1 << " and " << value2 << " returned " << (unsigned int)encoded << " while it should have returned " << (unsigned int)softpositSum);
 			}
-			counter++;
-			value1++;
-		} while (value1 != 0);
-		value2++;
-		if((value2%100) == 0){
+		}
+		if(((value2%100) == 0) and (value2 != 0)){
 			#pragma omp atomic
-			counter+=100;
+			counter+=(100*(1<<16));
 			#pragma omp critical
-			fprintf(stderr, "steps completed : %d\t/%lu\n", counter, TOTAL_TESTS);
+			fprintf(stderr, "\33[2K\rCompletion: \t%1.1f\%  (%lu\t/%lu)", ((double)counter/(double)TOTAL_TESTS)*100, counter,TOTAL_TESTS);
 		}
 		error_counter = 0;
-	}while (value2 != 0);
+	}
+	fprintf(stderr, "\33[2K\rCompletion: \t%1.1f\%  (%lu\t/%lu)\n", ((double)TOTAL_TESTS/(double)TOTAL_TESTS)*100, TOTAL_TESTS,TOTAL_TESTS);
 }
