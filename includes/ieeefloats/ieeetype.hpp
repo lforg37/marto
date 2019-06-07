165d5c8112286200c354992e92c61caeb424d0cb
Luc Forget <luc.forget@insa-lyon.fr>
2019-06-08 04:22:51 +0900

    Adding a IEEENumber type and dependency to hint api

---
+++ includes/ieeefloats/ieeetype.hpp
@@ -0,0 +1,42 @@
+#ifndef IEEETYPE_HPP
+#define IEEETYPE_HPP
+
+template<unsigned int WE, unsigned int WF, template<unsigned int, bool> class Wrapper>
+class IEEENumber : public Wrapper<WE + WF + 1, false>
+{
+	private:
+		typedef Wrapper<WE+WF+1, false> basetype;
+		template<unsigned int W>
+		using us_wrapper = Wrapper<W, false>;
+	public:
+		static constexpr unsigned int _WE = WE;
+		static constexpr unsigned int _WF = WF;
+
+		IEEENumber(Wrapper<WE + WF + 1, false> const & val):Wrapper<WE+WF+1, false>{val}{}
+
+		us_wrapper<1> getSign() {
+			return basetype::template get<WE+WF>();
+		}
+
+		us_wrapper<WE> getExponent() {
+			return basetype::template slice<WF + WE - 1, WF>();
+		}
+
+		us_wrapper<WF> getFractionnalPart() {
+			return basetype::template slice<WF - 1, 0>();
+		}
+
+		us_wrapper<1> getLeadBitVal() {
+			return getExponent().or_reduction();
+		}
+
+		us_wrapper<1> isInfinity() {
+			return getExponent().and_reduction().bitwise_and(getFractionnalPart().or_reduction().invert());
+		}
+
+		us_wrapper<1> isNaN() {
+			return getExponent().and_reduction().bitwise_and(getFractionnalPart().or_reduction());
+		}
+};
+
+#endif // IEEETYPE_HPP
