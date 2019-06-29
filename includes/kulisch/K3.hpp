d7146ec9e22fa7eade6f8d34668d1637a18cf743
UGUEN Yohann <yohann.uguen@inria.fr>
2019-06-29 09:48:09 +0200

    SMK3 splitting addition and subtraction

--- includes/kulisch/K3.hpp
+++ includes/kulisch/K3.hpp
@@ -173,18 +173,11 @@ ap_uint<bankSize> add_SMK3_to_add_Rec(
 )
 {
 	#pragma HLS INLINE
-	
-/*	if((stageIndex>=(stageSelect+getMantSpread<N, bankSize>()) && inputSign)){
-		return -1;
-	}
-	else{
-*/
 	return 0;
-//	}
 }
 
 template <int N, int bankSize, int spread>
-ap_uint<bankSize+bankSize> add_SMK3_to_add_Rec(
+ap_uint<bankSize> add_SMK3_to_add_Rec(
     ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
     ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
     ap_uint<1> inputSign,
@@ -194,26 +187,63 @@ ap_uint<bankSize+bankSize> add_SMK3_to_add_Rec(
 {	
 	#pragma HLS INLINE
 	ap_uint<bankSize> toAdd;
-	ap_uint<bankSize> toSub;
     if (stageIndex == (stageSelect+spread-1)) {
     	if(inputSign==1){
-    		toSub=shiftedSignificand.range(((spread)*bankSize)-1,(spread-1)*bankSize); 
     		toAdd=0;
     	}
     	else{
-    		toSub=0;
     		toAdd=shiftedSignificand.range(((spread)*bankSize)-1,(spread-1)*bankSize);
     	}
-        ap_uint<bankSize+bankSize> tmp = toAdd.concat(toSub); 
-        return tmp;
+        return toAdd;
     } else {
         return add_SMK3_to_add_Rec<N, bankSize, (spread-1)>(stageIndex, stageSelect, inputSign, shiftedSignificand);
     }
 }
 
 
+
 template <int N, int bankSize, int spread>
-ap_uint<bankSize+bankSize> add_SMK3_to_add_(
+ap_uint<bankSize> add_SMK3_to_sub_Rec(
+    ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
+    ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
+    ap_uint<1> inputSign,
+    ap_int<(1<<Static_Val<spread*bankSize>::_log2)> shiftedSignificand,
+    typename enable_if<(spread < 1)>::type* dummy = 0
+)
+{
+	#pragma HLS INLINE
+	return 0;
+}
+
+
+template <int N, int bankSize, int spread>
+ap_uint<bankSize> add_SMK3_to_sub_Rec(
+    ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
+    ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
+    ap_uint<1> inputSign,
+    ap_int<(1<<Static_Val<spread*bankSize>::_log2)> shiftedSignificand,
+    typename enable_if<(spread >= 1)>::type* dummy = 0
+    )
+{	
+	#pragma HLS INLINE
+	ap_uint<bankSize> toSub;
+    if (stageIndex == (stageSelect+spread-1)) {
+    	if(inputSign==1){
+    		toSub=shiftedSignificand.range(((spread)*bankSize)-1,(spread-1)*bankSize); 
+    	}
+    	else{
+    		toSub=0;
+    	}
+        return toSub;
+    } else {
+        return add_SMK3_to_sub_Rec<N, bankSize, (spread-1)>(stageIndex, stageSelect, inputSign, shiftedSignificand);
+    }
+}
+
+
+
+template <int N, int bankSize, int spread>
+ap_uint<bankSize> add_SMK3_to_add_(
     ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
     ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
     ap_uint<1> inputSign,
@@ -221,6 +251,61 @@ ap_uint<bankSize+bankSize> add_SMK3_to_add_(
 		){
 	#pragma HLS INLINE
 	return add_SMK3_to_add_Rec<N, bankSize, spread>(stageIndex, stageSelect, inputSign, shiftedSignificand);
+}
+
+
+
+
+template <int N, int bankSize, int spread>
+ap_uint<bankSize> add_SMK3_to_sub_(
+    ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
+    ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
+    ap_uint<1> inputSign,
+    ap_int<(1<<Static_Val<spread*bankSize>::_log2)	> shiftedSignificand
+		){
+	#pragma HLS INLINE
+	return add_SMK3_to_sub_Rec<N, bankSize, spread>(stageIndex, stageSelect, inputSign, shiftedSignificand);
+}
+
+
+
+template<int N, int bankSize>
+ap_uint<bankSize+1> add_SMK3_acc_sub(acc_SMK3<N, bankSize> acc, 
+										ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
+										ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
+										ap_int<(1<<Static_Val<getMantSpread<N, bankSize>()*bankSize>::_log2)> shiftedSignificand,
+										ap_uint<1> sign,
+										ap_uint<1> borrow0 =0)
+{
+	ap_uint<bankSize+1+1> bank = acc.getBank(stageIndex);
+	ap_uint<bankSize> toSub = add_SMK3_to_sub_<N, bankSize, getMantSpread<N, bankSize>()>(stageIndex, stageSelect, sign, shiftedSignificand);
+
+	ap_uint<bankSize+1> sub_op1{bank};
+	ap_uint<bankSize+1> sub_op2{toSub};
+	ap_uint<1> accBorrow = (stageIndex==0) ? borrow0 : acc.getBorrow(stageIndex-1);
+	ap_uint<bankSize+1>  sub = sub_op1 - sub_op2 - accBorrow;
+
+	return sub;	
+}
+
+template<int N, int bankSize>
+ap_uint<bankSize+1> add_SMK3_acc_add(acc_SMK3<N, bankSize> acc, 
+										ap_uint<Static_Val<getNbStages<N, bankSize>()>::_log2> stageIndex, 
+										ap_uint<FPDim<N>::WE+1 +1 - Static_Val<bankSize>::_log2 +1> stageSelect,
+										ap_int<(1<<Static_Val<getMantSpread<N, bankSize>()*bankSize>::_log2)> shiftedSignificand,
+										ap_uint<1> sign,
+										ap_uint<bankSize> subed_to_add,
+										ap_uint<1> carry0 =0)
+{
+	ap_uint<bankSize> toAdd = add_SMK3_to_add_<N, bankSize, getMantSpread<N, bankSize>()>(stageIndex, stageSelect, sign, shiftedSignificand);
+	ap_uint<1> accCarry = (stageIndex==0) ? carry0 : acc.getCarry(stageIndex-1);
+
+	ap_uint<bankSize+1> sum_op1{subed_to_add};
+	ap_uint<bankSize+1> sum_op2{toAdd};
+
+	ap_uint<bankSize+1> sum = sum_op1 + sum_op2 + accCarry;
+
+	return sum;	
 }
 
 template<int N, int bankSize>
@@ -234,29 +319,16 @@ ap_uint<bankSize+2> add_SMK3_acc_stage(acc_SMK3<N, bankSize> acc,
 {
 	#pragma HLS INLINE
 	#pragma HLS PIPELINE
-	ap_uint<bankSize+1+1> bank = acc.getBank(stageIndex);
+	
+	ap_uint<bankSize+1> subed = add_SMK3_acc_sub(acc, stageIndex, stageSelect, shiftedSignificand, sign, borrow0);
+	ap_uint<1> borrow = subed[bankSize];
+	ap_uint<bankSize> subed_to_add = subed.range(bankSize-1,0);
+
+	ap_uint<bankSize+1> added = add_SMK3_acc_add(acc, stageIndex, stageSelect, shiftedSignificand, sign, subed_to_add, carry0);
+	ap_uint<1> carry = added[bankSize];
+	ap_uint<bankSize> res = added.range(bankSize-1,0);
 	
 
-	ap_uint<bankSize+bankSize> toAdd_sub = add_SMK3_to_add_<N, bankSize, getMantSpread<N, bankSize>()>(stageIndex, stageSelect, sign, shiftedSignificand);
-
-	ap_uint<bankSize+1> sub_op1{bank};
-	ap_uint<bankSize+1> sub_op2{toAdd_sub.range(bankSize-1, 0)};
-	ap_uint<1> accBorrow = (stageIndex==0) ? borrow0 : acc.getBorrow(stageIndex-1);
-	ap_uint<bankSize+1>  sub = sub_op1 - sub_op2 - accBorrow;
-	ap_uint<1> borrow = sub[bankSize];
-
-
-
-	ap_uint<1> accCarry = (stageIndex==0) ? carry0 : acc.getCarry(stageIndex-1);
-	ap_uint<bankSize> to_sub = sub.range(bankSize-1,0);
-
-	ap_uint<bankSize+1> sum_op1{to_sub};
-	ap_uint<bankSize+1> sum_op2{toAdd_sub.range(bankSize+bankSize-1 ,bankSize)};
-
-	ap_uint<bankSize+1> sum = sum_op1 + sum_op2 + accCarry;
-	ap_uint<1> carry = sum[bankSize];
-
-	ap_uint<bankSize> res{sum.range(bankSize-1,0)};
 	ap_uint<1> finalCarry{carry and not(borrow)};
 	ap_uint<bankSize+1> resWithCarry{finalCarry.concat(res)};
 	ap_uint<1> finalBorrow{borrow and not(carry)};
