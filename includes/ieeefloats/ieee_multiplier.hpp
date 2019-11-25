177e464269346e73337b31eb5dc899e7f9995730
UGUEN Yohann <yohann.uguen@inria.fr>
2019-11-25 20:19:01 +0100

    Fixing IEEE multiplier

--- includes/ieeefloats/ieee_multiplier.hpp
+++ includes/ieeefloats/ieee_multiplier.hpp
@@ -33,16 +33,34 @@ IEEENumber<WE, WF, Wrapper> ieee_product(IEEENumber<WE, WF, Wrapper> i0, IEEENum
 #ifdef DEBUG
 	cerr << "i0: " << to_string(i0) << endl;
 	cerr << "i1: " << to_string(i1) << endl;
-	cerr << "i0 sign: " << to_string(i0.getSign()) << endl;
-	cerr << "i1 sign: " << to_string(i1.getSign()) << endl;
 #endif
+
+
+	auto i0_exp = i0.getExponent();
+	auto i0_exp_adjusted = i0_exp.template slice<WE-1,1>().concatenate(i0_exp.template get<0>().bitwise_or(i0_is_subnormal));
+	auto i1_exp = i1.getExponent();
+	auto i1_exp_adjusted = i1_exp.template slice<WE-1,1>().concatenate(i1_exp.template get<0>().bitwise_or(i1_is_subnormal));
 
 	auto i0_imp_significand = i0.getLeadBitVal().concatenate(i0.getFractionnalPart());
 	auto i1_imp_significand = i1.getLeadBitVal().concatenate(i1.getFractionnalPart());
 
+	auto sign = i0.getSign() xor i1.getSign();
+	Wrapper<WE+1, false> exp_sum = i0_exp_adjusted.addWithCarry(i1_exp_adjusted, Wrapper<1, false>{0});
+	Wrapper<2*WF+2, false> significand_prod = (i0_imp_significand) * (i1_imp_significand);
+
+	Wrapper<WE+2, false> FP_BIAS{(1<<(WE-1))-1};
+
 #ifdef DEBUG
+	cerr << "i0 sign: " << to_string(i0.getSign()) << endl;
+	cerr << "i1 sign: " << to_string(i1.getSign()) << endl;
+	cerr << "i0 exp: " << to_string(i0_exp) << endl;
+	cerr << "i1 exp: " << to_string(i1_exp) << endl;
 	cerr << "i0 mantissa: " << to_string(i0_imp_significand) << endl;
 	cerr << "i1 mantissa: " << to_string(i1_imp_significand) << endl;
+	cerr << "FP_BIAS " << to_string(FP_BIAS) << endl;
+	cerr << "sign: " << to_string(sign) << endl;
+	cerr << "exp_sum: " << to_string(exp_sum) << endl;
+	cerr << "significand_prod " << to_string(significand_prod) << endl;
 #endif
 
 	auto entry_to_lzc = Wrapper<WF+1, false>::mux(i0_is_subnormal,
@@ -52,120 +70,135 @@ IEEENumber<WE, WF, Wrapper> ieee_product(IEEENumber<WE, WF, Wrapper> i0, IEEENum
 	auto lzc_subnormal = lzoc_wrapper(entry_to_lzc, Wrapper<1, false>{0});
 
 #ifdef DEBUG
+	cerr << "to_lzc: " << to_string(entry_to_lzc) << endl;
 	cerr << "lzc_subnormal: " << to_string(lzc_subnormal) << endl;
 #endif
 
-
-
-	auto sign = i0.getSign() xor i1.getSign();
-
-#ifdef DEBUG
-	cerr << "sign: " << to_string(sign) << endl;
-#endif
-
-	Wrapper<WE+2, false> FP_BIAS{(1<<(WE-1))-1};
-
-#ifdef DEBUG
-	cerr << "FP_BIAS " << to_string(FP_BIAS) << endl;
-	cerr << "i0 exp: \t " << to_string(i0.getExponent()) << endl;
-	cerr << "i1 exp: \t " << to_string(i1.getExponent()) << endl;
-#endif
-
-	Wrapper<WE+1, false> exp_sum = i0.getExponent().addWithCarry(i1.getExponent(), Wrapper<1, false>{0});
-	Wrapper<WE+1, false> exp_sum_plus_one = i0.getExponent().addWithCarry(i1.getExponent(), Wrapper<1, false>{1});
-	Wrapper<2*WF+2, false> significand_prod = (i0_imp_significand) * (i1_imp_significand);
-
-	Wrapper<WE+1, false> exp_sum_to_take = Wrapper<WE+1, false>::mux(significand_prod.template get<2*WF+2-1>(), exp_sum_plus_one, exp_sum);
-#ifdef DEBUG
-	cerr << "exp_sum_to_take " << to_string(exp_sum_to_take) << endl;
-#endif
-	Wrapper<WE+2, false> ext_exp_sum = Wrapper<1, false>{0}.concatenate(exp_sum_to_take);
+	Wrapper<WE+2, false> ext_exp_sum = Wrapper<1, false>{0}.concatenate(exp_sum);
 
 	auto unbiased_exp = ext_exp_sum.modularSub(FP_BIAS);
 	auto unbiased_is_neg = unbiased_exp.template get<WE+2-1>();
-	auto threshold = (Wrapper<2, false>{0}.concatenate(Wrapper<WE, false>::generateSequence({1})));
-	auto overflow = (unbiased_exp>=threshold).bitwise_and(unbiased_exp.template get<WE+2-1>().invert());
-#ifdef DEBUG
-	cerr << "threshold " << to_string(threshold) << endl;
-	cerr << "overflow " << to_string(overflow) << endl;
-#endif
-
-	auto to_add_to_right_shift = significand_prod.template get<2*WF+2-1>().concatenate(
-							significand_prod.template get<2*WF+2-2>().bitwise_and(significand_prod.template get<2*WF+2-1>().invert()).bitwise_and(i0_is_subnormal.bitwise_or(i1_is_subnormal).invert())
-							);
-#ifdef DEBUG
-	cerr << "to_add_to_right_shift " << to_string(to_add_to_right_shift) << endl;
-#endif
-
-
-	auto right_shift_value = Wrapper<WE, false>{0}.concatenate(to_add_to_right_shift).modularSub(unbiased_exp);
+	// (
+	// 					Wrapper<2, false>{0}.concatenate(
+	// 											Wrapper<WE-1, false>::generateSequence({1})
+	// 										).concatenate(
+	// 											Wrapper<1, false>::generateSequence({0})
+	// 										)
+	// 				);
 
 
 
-	auto final_exp = Wrapper<WE, false>::mux(unbiased_is_neg,
-												Wrapper<WE, false>{0},
-												unbiased_exp.template slice<WE-1,0>()
-											);
+	auto right_shift_value = Wrapper<WE+2, false>{1}.modularSub(unbiased_exp);
 
+
+
+	auto unbiased_minus_1 = unbiased_exp.modularSub(Wrapper<WE+2,false>{1});
+	auto unbiased_minus_1_red = unbiased_minus_1.template slice<WE-1,0>();
+	// auto early_of = unbiased_minus_1.template slice<WE+2-1,WE>().or_reduction();
+	auto unbiased_minus_1_is_neg = unbiased_minus_1.template get<WE+2-1>();
 	auto ext_lzc = lzc_subnormal.template leftpad<WE>();
-	auto left_shift_value = Wrapper<WE, false>::mux(ext_lzc>final_exp,
-												final_exp,
+	auto left_shift_value = Wrapper<WE, false>::mux(ext_lzc>unbiased_minus_1_red,
+												unbiased_minus_1_red,
 												ext_lzc);
 
+
+	auto final_exp = Wrapper<WE, false>::mux(unbiased_minus_1_is_neg,
+												Wrapper<WE, false>{0},
+												unbiased_minus_1_red
+											);
 #ifdef DEBUG
-	cerr << "unbiased_exp " << to_string(unbiased_exp) << endl;
-	cerr << "right_shift_value " << to_string(right_shift_value) << endl;
+	cerr << "unbiased_minus_1 " << to_string(unbiased_minus_1) << endl;
+	cerr << "ext_lzc " << to_string(ext_lzc) << endl;
+	cerr << "final_exp " << to_string(final_exp) << endl;
 	cerr << "left_shift_value " << to_string(left_shift_value) << endl;
+	cerr << "unbiased_exp " << to_string(unbiased_exp) << endl;
 #endif
 
-	auto adjusted_exp_if_sub = Wrapper<WE, false>::mux(i0_is_subnormal.bitwise_or(i1_is_subnormal).bitwise_and(unbiased_is_neg.invert()),
-														final_exp.modularSub(left_shift_value),
-														final_exp);
-	auto adjusted_exp_if_zero = Wrapper<WE, false>::mux(i0_is_zero.bitwise_or(i1_is_zero),
-														Wrapper<WE, false>{0},
-														adjusted_exp_if_sub);
-
-	auto must_left_shift = i0_is_subnormal.bitwise_or(i1_is_subnormal);
+	auto must_right_shift = unbiased_minus_1.template get<WE+2-1>();
+	auto must_left_shift = must_right_shift.invert();
 	auto left_shift = Wrapper<WE+2, false>::mux(must_left_shift,
 		Wrapper<WE+2, false>{0}.modularSub(left_shift_value.template leftpad<WE+2>()),
 		Wrapper<WE+2, false>{0}
 		);
 
-	auto must_right_shift = unbiased_is_neg.bitwise_or(unbiased_exp.or_reduction().invert());
+	// auto must_right_shift = must_left_shift.invert();
+	// unbiased_is_neg.bitwise_or(unbiased_exp.or_reduction().invert());
 	auto right_shift = Wrapper<WE+2, false>::mux(must_right_shift,
 		right_shift_value,
 		left_shift
 		);
+
+#ifdef DEBUG
+	cerr << "must_right_shift: \t\t\t" << to_string(must_right_shift) << endl;
+	cerr << "must_left_shift: \t\t\t" << to_string(must_left_shift) << endl;
+	cerr << "right_shift_value: \t\t\t" << to_string(right_shift_value) << endl;
+	cerr << "left_shift: \t\t\t" << to_string(left_shift) << endl;
+	cerr << "to sub to : \t\t\t" << to_string(Wrapper<WE+2, false>{2*WF+3-(WF+3)}) << endl;	
+#endif
 
 	auto shift_value = Wrapper<WE+2, false>{2*WF+3-(WF+3)}.modularAdd(right_shift);
 	auto right_shifted_with_sticky = hint::shifter_sticky(significand_prod.concatenate(Wrapper<1, false>{0}), shift_value, Wrapper<1, false>{0});
 	auto right_shifted = right_shifted_with_sticky.template slice<2*WF+2+1-1,1>();
 	auto sticky_from_shift = right_shifted_with_sticky.template get<0>();
 
-	auto lead_exact_prod = right_shifted.template get<WF+2>();
-	auto second_lead_exact_prod = right_shifted.template get<WF+2-1>();
-	auto final_significand = Wrapper<WF, false>::mux(lead_exact_prod,
-														right_shifted.template slice<WF+1,2>(),
-														right_shifted.template slice<WF,1>()
-														);
-	auto extra_sticky_bit = Wrapper<1, false>::mux(lead_exact_prod,
-														right_shifted.template get<0>(),
-														Wrapper<1, false>{0}
-														);
-	auto final_sticky = sticky_from_shift.bitwise_or(extra_sticky_bit);
-	auto round_bit = Wrapper<1, false>::mux(lead_exact_prod,
-														right_shifted.template get<1>(),
-														right_shifted.template get<0>()
-														);
-	auto lsb = Wrapper<1, false>::mux(lead_exact_prod,
-													right_shifted.template get<2>(),
-													right_shifted.template get<1>()
-													);
-	auto to_sum_for_rounding = round_bit.bitwise_and(final_sticky).bitwise_or(round_bit.bitwise_and(final_sticky.invert()).bitwise_and(lsb));
-
-
 #ifdef DEBUG
-	cerr << "final_sticky: \t\t\t" << to_string(final_sticky) << endl;
+	cerr << "shift_input: \t\t\t" << to_string(significand_prod.concatenate(Wrapper<1, false>{0})) << endl;
+	cerr << "shift_value: \t\t\t" << to_string(shift_value) << endl;
+	cerr << "right_shifted_with_sticky: \t" << to_string(right_shifted_with_sticky) << endl;
+	cerr << "right_shifted: \t\t\t" << to_string(right_shifted) << endl;
+	cerr << "sticky_from_shift: \t\t\t" << to_string(sticky_from_shift) << endl;
+#endif
+
+	auto needs_extra_shift = right_shifted.template get<WF+2>();
+	auto lead_exact_prod = Wrapper<1,false>::mux(needs_extra_shift,
+												needs_extra_shift,
+												right_shifted.template get<WF+2-1>()
+											);
+
+	auto final_significand = Wrapper<WF,false>::mux(needs_extra_shift,
+												right_shifted.template slice<WF+1,2>(),
+												right_shifted.template slice<WF,1>()
+											);
+
+	auto round_bit = Wrapper<1,false>::mux(needs_extra_shift,
+												right_shifted.template get<1>(),
+												right_shifted.template get<0>()
+											);
+
+	auto lsb = Wrapper<1,false>::mux(needs_extra_shift,
+												right_shifted.template get<2>(),
+												right_shifted.template get<1>()
+											);
+
+	auto sticky = Wrapper<1,false>::mux(needs_extra_shift,
+												sticky_from_shift.bitwise_or(right_shifted.template get<0>()),
+												sticky_from_shift
+											);
+
+	auto to_sum_for_rounding = round_bit.bitwise_and(sticky).bitwise_or(round_bit.bitwise_and(sticky.invert()).bitwise_and(lsb));
+	auto scaled_exp = final_exp.addWithCarry(Wrapper<WE, false>{0}, Wrapper<1, false>{1});
+	auto adjusted_exp_if_sub = Wrapper<WE+1, false>::mux(i0_is_subnormal.bitwise_or(i1_is_subnormal).bitwise_and(must_left_shift),
+														scaled_exp.modularSub(left_shift_value.template leftpad<WE+1>()),
+														scaled_exp);
+	auto adjusted_exp_if_rebecomes_normal = Wrapper<WE+1, false>::mux(adjusted_exp_if_sub.template slice<WE,1>().or_reduction().invert().bitwise_and(adjusted_exp_if_sub.template get<0>()).bitwise_and(lead_exact_prod.invert()),
+														Wrapper<WE+1, false>{0},
+														adjusted_exp_if_sub
+														);
+
+	auto adjusted_exp_extra_shift = Wrapper<WE+2, false>::mux(needs_extra_shift,
+														adjusted_exp_if_rebecomes_normal.addWithCarry(Wrapper<WE+1, false>{0}, Wrapper<1, false>{1}),
+														Wrapper<1, false>{0}.concatenate(adjusted_exp_if_rebecomes_normal)
+														);
+	auto threshold = Wrapper<WE+2, false>{(1<<WE)-1};
+	auto overflow = (adjusted_exp_extra_shift>=threshold).bitwise_or(unbiased_minus_1.template get<WE+1>().invert().bitwise_and(unbiased_minus_1.template get<WE>()));
+#ifdef DEBUG
+	cerr << "scaled_exp " << to_string(scaled_exp) << endl;
+	cerr << "threshold " << to_string(threshold) << endl;
+	cerr << "overflow " << to_string(overflow) << endl;
+#endif
+#ifdef DEBUG
+
+	cerr << "lead_exact_prod: \t\t" << to_string(lead_exact_prod) << endl;
 	cerr << "round_bit: \t\t\t" << to_string(round_bit) << endl;
 	cerr << "lsb: \t\t\t\t" << to_string(lsb) << endl;
 	cerr << "to_sum_for_rounding: \t\t" << to_string(to_sum_for_rounding) << endl;
@@ -173,31 +206,13 @@ IEEENumber<WE, WF, Wrapper> ieee_product(IEEENumber<WE, WF, Wrapper> i0, IEEENum
 
 
 
-#ifdef DEBUG
-	cerr << "base: \t\t\t" << to_string(Wrapper<WE+2, false>{2*WF+2-(WF+3)}) << endl;
-	cerr << "add1: \t\t\t" << to_string(significand_prod.template get<2*WF+2-1>()) << endl;
-	cerr << "left: \t\t\t" << to_string(left_shift) << endl;
-	cerr << "right: \t\t\t" << to_string(right_shift) << endl;
-	cerr << "shift_value: \t\t\t" << to_string(shift_value) << endl;
-	cerr << "right_shifted: \t\t\t" << to_string(right_shifted) << endl;
-	cerr << "final_significand: \t\t\t" << to_string(final_significand) << endl;
-#endif
-
-
-	auto adjusted_exp_if_rebecomes_normal = Wrapper<WE, false>::mux(i0_is_subnormal.bitwise_or(i1_is_subnormal),
-														adjusted_exp_if_zero.modularAdd(Wrapper<WE-2, false>::generateSequence({0}).concatenate(lead_exact_prod).concatenate(lead_exact_prod.invert().bitwise_and(second_lead_exact_prod))),
-														adjusted_exp_if_zero);
-
-	auto rounded_result = sign.concatenate(adjusted_exp_if_rebecomes_normal).concatenate(final_significand).modularAdd(to_sum_for_rounding.template leftpad<WE+WF+1>());
+	auto rounded_result = sign.concatenate(adjusted_exp_extra_shift.template slice<WE-1,0>()).concatenate(final_significand).modularAdd(to_sum_for_rounding.template leftpad<WE+WF+1>());
 
 #ifdef DEBUG
-	cerr << "bias: \t\t" << to_string(FP_BIAS) << endl;
-	cerr << "unbiased_exp: \t" << to_string(unbiased_exp) << endl;
-	cerr << "final_exp: \t " << to_string(final_exp) << endl;
+	cerr << "rounded_result: \t " << to_string(rounded_result) << endl;
 	cerr << "adjusted_exp_if_sub: \t " << to_string(adjusted_exp_if_sub) << endl;
-	cerr << "adjusted_exp_if_zero: \t " << to_string(adjusted_exp_if_zero) << endl;
 	cerr << "adjusted_exp_if_rebecomes_normal: \t " << to_string(adjusted_exp_if_rebecomes_normal) << endl;
-	cerr << "significand prod: " << to_string(significand_prod) << endl;
+	cerr << "adjusted_exp_extra_shift: \t " << to_string(adjusted_exp_extra_shift) << endl;
 	cerr << "final_significand: " << to_string(final_significand) << endl;
 #endif
 
@@ -212,9 +227,13 @@ IEEENumber<WE, WF, Wrapper> ieee_product(IEEENumber<WE, WF, Wrapper> i0, IEEENum
 	cerr << "result_is_infty: \t" << to_string(result_is_infty) << endl;
 #endif
 
+	auto result_if_zero = Wrapper<1+WE+WF, false>::mux(i0_is_zero.bitwise_or(i1_is_zero),
+														sign.concatenate(Wrapper<WE+WF, false>{0}),
+														rounded_result
+														);
 	auto result_if_NaN = Wrapper<1+WE+WF, false>::mux(result_is_NaN,
 														NaN,
-														rounded_result
+														result_if_zero
 														);
 	auto result_if_infty = Wrapper<1+WE+WF, false>::mux(result_is_infty,
 													signed_infty,
--- tests/testIEEEMultiplier.cpp
+++ tests/testIEEEMultiplier.cpp
@@ -33,55 +33,55 @@ BOOST_AUTO_TEST_CASE(TestIEEEMul0)
 
 	auto c = a*b;
 
-	BOOST_REQUIRE_MESSAGE(c.getRepr()==0, "Error");
+	BOOST_REQUIRE_MESSAGE(c.getRepr()==1, "Error");
 }
 
 
 BOOST_AUTO_TEST_CASE(TestIEEMul_3_4)
 {
-	constexpr unsigned int WE = 3;
-	constexpr unsigned int WF = 4;
+	constexpr unsigned int WE = 4;
+	constexpr unsigned int WF = 7;
 
 	using SIEEE = libnumform::SmallIEEENumber<WE, WF>;
 
 
-	// half_float::half var1, var2, half_prod;
-	// uint16_t var2_ui16 = 0b0000000000000011;
-	// uint16_t var1_ui16 = 0b0110000101010110;
+	// uint16_t var1_ui16 = 0b000001001;
+	// uint16_t var2_ui16 = 0b001010111;
+	// SIEEE op1{var1_ui16}, op2{var2_ui16};
+	// auto prod = op1 * op2;
+	// auto prod_repr = prod.getRepr();
+	// std::bitset<9> x1(var1_ui16);
+	// std::bitset<9> x2(var2_ui16);
 
-	// var1.data_ = var1_ui16;
-	// var2.data_ = var2_ui16;
-	// std::bitset<16> x1(var1_ui16);
-	// std::bitset<16> x2(var2_ui16);
-	// cerr << "var1 " << x1 << endl;
-	// cerr << "var2 " << x2 << endl;
-	// half_prod = var1 * var2;
-	// ap_uint<16> resgmp{half_prod.data_};
-	// IEEENumber<WE, WF, hint::VivadoWrapper> ieee_res_gmp{resgmp};
+	// hint::GMPWrapper<9, false> resgmp{prod.getRepr()};
+	// IEEENumber<WE, WF, hint::GMPWrapper> ieee_res_gmp{resgmp};
 
-	// ap_uint<16> var1gmp{var1_ui16};
-	// ap_uint<16> var2gmp{var2_ui16};
-	// IEEENumber<WE, WF, hint::VivadoWrapper> var1ieee{var1gmp};
-	// IEEENumber<WE, WF, hint::VivadoWrapper> var2ieee{var2gmp};
+	// hint::GMPWrapper<9, false> var1gmp{var1_ui16};
+	// hint::GMPWrapper<9, false> var2gmp{var2_ui16};
+	// IEEENumber<WE, WF, hint::GMPWrapper> var1ieee{var1gmp};
+	// IEEENumber<WE, WF, hint::GMPWrapper> var2ieee{var2gmp};
 	// auto marto_prod = ieee_product(var1ieee, var2ieee);
 
-	// cerr << "half \t" << hint::to_string(static_cast<hint::VivadoWrapper<16, false> >(ieee_res_gmp) ) << endl;
-	// cerr << "marto \t" << hint::to_string(static_cast<hint::VivadoWrapper<16, false> >(marto_prod) ) << endl;
+	// cerr << "SIEEE \t" << hint::to_string(static_cast<hint::GMPWrapper<9, false> >(ieee_res_gmp) ) << endl;
+	// cerr << "marto \t" << hint::to_string(static_cast<hint::GMPWrapper<9, false> >(marto_prod) ) << endl;
+
+	// exit(0);
 
 
 	// omp_set_num_threads(16);
 	constexpr unsigned int FORMAT_SIZE = 1 + WE + WF;
 	constexpr uint32_t FORMAT_LIMIT = 1 << FORMAT_SIZE;
-	SIEEE op1{0}, op2{0};
 
-	//#pragma omp parallel for
+	#pragma omp parallel for
 	for (uint32_t count1 = 0 ; count1 < FORMAT_LIMIT ; ++count1) {
+		SIEEE op1{0};
 		uint32_t op1_repr = count1;
 		mpz_class var1gmp{op1_repr};
 		IEEENumber<WE, WF, hint::GMPWrapper> var1ieee{var1gmp};
 		op1 = op1_repr;
 
 		for (uint32_t count2=0 ; count2 < FORMAT_LIMIT ; count2++ ) {
+			SIEEE op2{0};
 			std::cerr << count1 << ", " << count2 << endl;
 			uint32_t op2_repr = count2;
 			mpz_class var2gmp{op2_repr};
@@ -103,7 +103,7 @@ BOOST_AUTO_TEST_CASE(TestIEEMul_3_4)
 				// bool must = ((marto_prod.unravel()-resgmp) <= 1) and ((resgmp-marto_prod.unravel()) <= 1);
 				if(not must){
 					//#pragma omp critical
-					BOOST_REQUIRE_MESSAGE(false, "Error for" <<
+					BOOST_REQUIRE_MESSAGE(false, "Error for " <<
 										  hint::to_string(static_cast<GMPWrapper<FORMAT_SIZE, false> >(var1ieee) ) <<
 										  " and " << hint::to_string(static_cast<GMPWrapper<FORMAT_SIZE, false> >(var2ieee) ) <<
 										  "\nexpecting\t" << hint::to_string(GMPWrapper<FORMAT_SIZE, false>{prod_repr} ) <<
