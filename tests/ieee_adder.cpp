baa7b87f765708987b58a0b09cd624f351e64ead
Luc Forget <luc.forget@insa-lyon.fr>
2020-09-14 16:00:42 +0200

    ieee_adder.cpp

---
+++ tests/ieee_adder.cpp
@@ -0,0 +1,25 @@
+#define VIVADO_BACKEND
+
+#include <ap_int.h>
+#include "hint.hpp"
+#include "ieeefloats/ieeetype.hpp"
+#include "ieeefloats/ieee_adder.hpp"
+
+constexpr unsigned int WE = IEEE_WE;
+constexpr unsigned int WF = IEEE_WF;
+constexpr unsigned int ENC_SIZE = WE + WF + 1;
+
+ap_uint<ENC_SIZE> ieee_comp_add(
+	IEEENumber<WE, WF, hint::VivadoWrapper> in1,
+	IEEENumber<WE, WF, hint::VivadoWrapper> in2
+)
+{
+	auto add = ieee_add_sub_impl(in1, in2);
+	return add.unravel();
+}
+
+int main(void)
+{
+	ieee_comp_add({{17}}, {{78}});
+	return 0;
+}
