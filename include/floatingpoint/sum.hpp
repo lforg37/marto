c5e52dcd6ff5457c6cd4daa19e56cb7fa9cbe7e6
Luc Forget <luc.forget@insa-lyon.fr>
2020-12-06 23:34:38 +0100

    Adding a few tests on standard value summation

--- include/floatingpoint/sum.hpp
+++ include/floatingpoint/sum.hpp
@@ -2,10 +2,20 @@
 #define SUM_HPP
 
 #include <primitives/lzoc_shifter.hpp>
+#include <primitives/shifter.hpp>
 #include <tools/static_math.hpp>
 
 using hint::LZOC_shift;
 using hint::Static_Val;
+using hint::shifter;
+
+#ifdef FPEXPR_SUM_DEBUG
+#include <iostream>
+#include "tools/printing.hpp"
+using hint::to_string;
+using std::cerr;
+#endif
+
 
 #include "fp_number.hpp"
 
@@ -77,6 +87,9 @@ struct RoundedFPSum
 			auto signif1 = op1.getSignificand().template rightpad<max_wf+1>();
 			auto signif2 = op2.getSignificand().template rightpad<max_wf+1>();
 
+			auto negSignif1 = op1.getSignificand().template rightpad<max_wf+1>().invert().modularAdd({{1}});
+			auto negSignif2 = op2.getSignificand().template rightpad<max_wf+1>().invert().modularAdd({{1}});
+
 			auto exp1_greater = exp1 > exp2;
 			auto exp_equals = (exp1 == exp2);
 
@@ -97,7 +110,9 @@ struct RoundedFPSum
 
 
 			auto g_signif = Wrapper<max_wf+1, false>::mux(op1_greater, signif1, signif2).template rightpad<sum_op_align + 2>();
-			auto l_signif = Wrapper<max_wf+1, false>::mux(op1_greater, signif2, signif1).template rightpad<sum_op_align + 2>();
+			auto l_signif_pos = Wrapper<max_wf+1, false>::mux(op1_greater, signif2, signif1);
+			auto l_signif_neg = Wrapper<max_wf+1, false>::mux(op1_greater, negSignif2, negSignif1);
+			auto l_signif = Wrapper<max_wf+1, false>::mux(eff_sub, l_signif_neg, l_signif_pos).template rightpad<sum_op_align + 2>();
 
 			//------ Flags ----------------------------------------------------------------------------------------------------/
 
@@ -108,18 +123,18 @@ struct RoundedFPSum
 
 			//------ Close path ------------------------------------------------------------------------------------------------
 			// Handle only the cancellation case
-			auto l_close = l_signif >> (exp_equals.invert());
-			auto close_add = g_signif.modularSub(l_close);
+			auto l_close = (Wrapper<1, false>{{1}}.concatenate(l_signif) >> (exp_equals.invert())).template slice <sum_op_align+1, 0>();
+			auto close_add = g_signif.modularAdd(l_close);
 			auto lzoc_shifted = LZOC_shift<sum_op_align+2, lzoc_count>(close_add, {{0}});
 			auto lzoc = lzoc_shifted.lzoc;
 			auto cancellation_frac = lzoc_shifted.shifted.template slice<sum_op_align, 0>();
 			auto cancellation_exp = g_exp.modularSub(lzoc.template leftpad<partial_we>().as_signed()).as_signed();
-			auto cancel_to_zero = (lzoc == Wrapper<lzoc_size, false>{{sum_op_align + 2}});
+			auto cancel_to_zero = lzoc_shifted.shifted.template get<sum_op_align + 1>().invert();
 
 			//------ Far Path ---------------------------------------------------------------------------------------------------
-			auto frac_mask = Wrapper<sum_op_align+3, false>::generateSequence(eff_sub);
-			auto add_res = g_signif.template rightpad<sum_op_align + 3>().addWithCarry((l_signif.template rightpad<sum_op_align + 3>() >> exp_diff) ^ frac_mask, eff_sub);
-			auto overflowed = add_res.template get<sum_op_align + 3>();
+			auto shifted_low_frac = shifter<true>(l_signif.template rightpad<sum_op_align + 3>(), exp_diff, eff_sub);
+			auto add_res = g_signif.template rightpad<sum_op_align + 3>().addWithCarry(shifted_low_frac, {{0}});
+			auto overflowed = add_res.template get<sum_op_align + 3>() & eff_sub.invert();
 			auto normal_pos = add_res.template get<sum_op_align + 2>() & overflowed.invert();
 			auto shiftval = overflowed.concatenate(normal_pos);
 			auto frac_ext = add_res.template slice<sum_op_align + 2, 0>() >> shiftval;
@@ -134,6 +149,62 @@ struct RoundedFPSum
 			auto final_exp = Wrapper<partial_we, true>::mux(select_close_path, cancellation_exp, expNormal);
 
 			auto isZero = isZeroFromFlags | (select_close_path & cancel_to_zero);
+
+#ifdef FPEXPR_SUM_DEBUG
+			cerr << "==== FPEXPR SUM ======" << endl;
+			cerr << "op1IsZero: " << to_string(op1IsZero) << endl;
+			cerr << "op2IsZero: " << to_string(op2IsZero) << endl;
+			cerr << "isInf1: " << to_string(isInf1) << endl;
+			cerr << "isInf2: " << to_string(isInf2) << endl;
+			cerr << "exp1: " << to_string(exp1) << endl;
+			cerr << "exp2: " << to_string(exp2) << endl;
+			cerr << "diff1: " << to_string(diff1) << endl;
+			cerr << "diff2: " << to_string(diff2) << endl;
+			cerr << "signif1: " << to_string(signif1) << endl;
+			cerr << "signif2: " << to_string(signif2) << endl;
+			cerr << "negSignif1: " << to_string(negSignif1) << endl;
+			cerr << "negSignif2: " << to_string(negSignif2) << endl;
+			cerr << "exp1_greater: " << to_string(exp1_greater) << endl;
+			cerr << "exp_equals: " << to_string(exp_equals) << endl;
+			cerr << "frac1_greater: " << to_string(frac1_greater) << endl;
+			cerr << "op1_greater: " << to_string(op1_greater) << endl;
+			cerr << "exp_diff: " << to_string(exp_diff) << endl;
+			cerr << "exp_diff_is_one: " << to_string(exp_diff_is_one) << endl;
+			cerr << "s1: " << to_string(s1) << endl;
+			cerr << "s2: " << to_string(s2) << endl;
+			cerr << "eff_sub: " << to_string(eff_sub) << endl;
+			cerr << "g_exp: " << to_string(g_exp) << endl;
+			cerr << "s_res: " << to_string(s_res) << endl;
+			cerr << "g_signif: " << to_string(g_signif) << endl;
+			cerr << "l_signif_pos: " << to_string(l_signif_pos) << endl;
+			cerr << "l_signif_neg: " << to_string(l_signif_neg) << endl;
+			cerr << "l_signif: " << to_string(l_signif) << endl;
+			cerr << "bothInf: " << to_string(bothInf) << endl;
+			cerr << "isNaN: " << to_string(isNaN) << endl;
+			cerr << "isInf: " << to_string(isInf) << endl;
+			cerr << "isZeroFromFlags: " << to_string(isZeroFromFlags) << endl;
+			cerr << "l_close: " << to_string(l_close) << endl;
+			cerr << "close_add: " << to_string(close_add) << endl;
+			cerr << "lzoc: " << to_string(lzoc) << endl;
+			cerr << "cancellation_frac: " << to_string(cancellation_frac) << endl;
+			cerr << "cancellation_exp: " << to_string(cancellation_exp) << endl;
+			cerr << "cancel_to_zero: " << to_string(cancel_to_zero) << endl;
+			cerr << "shifted_low_frac: " << to_string(shifted_low_frac) << endl;
+			cerr << "add_res: " << to_string(add_res) << endl;
+			cerr << "overflowed: " << to_string(overflowed) << endl;
+			cerr << "normal_pos: " << to_string(normal_pos) << endl;
+			cerr << "shiftval: " << to_string(shiftval) << endl;
+			cerr << "frac_ext: " << to_string(frac_ext) << endl;
+			cerr << "fracRes: " << to_string(fracRes) << endl;
+			cerr << "expMask: " << to_string(expMask) << endl;
+			cerr << "expNormal: " << to_string(expNormal) << endl;
+			cerr << "select_close_path: " << to_string(select_close_path) << endl;
+			cerr << "final_frac: " << to_string(final_frac) << endl;
+			cerr << "final_exp: " << to_string(final_exp) << endl;
+			cerr << "isZero: " << to_string(isZero) << endl;
+
+			cerr << "============" << endl;
+#endif
 
 			return FPNumber<partial_sum_dim, Wrapper>(
 						final_frac,
--- tests/testExpression.cpp
+++ tests/testExpression.cpp
@@ -107,61 +107,357 @@ BOOST_AUTO_TEST_CASE(TestProduct)
 
 #ifdef MPFR
 template<typename Dim1, typename Dim2, vec_width targetWF>
-void check_addition_standard(int64_t exp1, int64_t exp2, int64_t sfrac1, int64_t sfrac2)
+inline bool check_addition_standard(int64_t s1, int64_t s2, int64_t exp1, int64_t exp2, int64_t frac1, int64_t frac2)
 {
-	int sign1 = (sfrac1 < 0) ? 1 : 0;
-	int sign2 = (sfrac2 < 0) ? 1 : 0;
-	uint64_t frac1 = (sfrac1 < 0) ? -sfrac1 : sfrac1;
-	uint64_t frac2 = (sfrac2 < 0) ? -sfrac2 : sfrac2;
-
 	/**** Using expr ********/
 
 	using fpnum1 = FPNumber<Dim1, VivadoWrapper>;
 	using fpnum2 = FPNumber<Dim2, VivadoWrapper>;
 
-	fpnum1 op1{{{frac1}}, {{exp1}}, {{sign1}}, {{0}}, {{0}}, {{0}}};
-	fpnum2 op2{{{frac2}}, {{exp2}}, {{sign2}}, {{0}}, {{0}}, {{0}}};
+	fpnum1 op1{{{frac1}}, {{exp1}}, {{s1}}, {{0}}, {{0}}, {{0}}};
+	fpnum2 op2{{{frac2}}, {{exp2}}, {{s2}}, {{0}}, {{0}}, {{0}}};
 
 	auto eop1 = to_expr(op1);
 	auto eop2 = to_expr(op2);
 
 	auto exprres = eop1+eop2;
 	auto res = exprres.template computeWithTargetPrecision<targetWF>();
+	bool res_is_zero = res.isZero().unravel();
 
 	/**** Using mpfr ****/
-	mpfr_t mpfr_op1, mpfr_op2, mpfr_res;
-	mpfr_init2(mpfr_op1, Dim1::WF);
-	mpfr_init2(mpfr_op2, Dim2::WF);
-	mpfr_init2(mpfr_res, targetWF);
+	mpfr_t mpfr_op1, mpfr_op2, mpfr_sum, mpfr_round;
+	mpfr_init2(mpfr_op1, Dim1::WF + 1);
+	mpfr_init2(mpfr_op2, Dim2::WF + 1);
+	mpfr_init2(mpfr_sum, targetWF + 2);
+	mpfr_init2(mpfr_round, targetWF + 2);
 
-	uint64_t signif1 = frac1 | (uint64_t{1} << Dim1::WF);
-	uint64_t signif2 = frac2 | (uint64_t{1} << Dim2::WF);
-
+	constexpr uint64_t signif_mask1 = {1 << Dim1::WF};
+	uint64_t signif1 = frac1 | signif_mask1;
 	mpfr_set_ui_2exp(mpfr_op1, signif1, exp1 - Dim1::WF, MPFR_RNDN);
-	mpfr_set_ui_2exp(mpfr_op2, signif2, exp2 - Dim2::WF, MPFR_RNDN);
-	mpfr_setsign(mpfr_op1, mpfr_op1, sign1, MPFR_RNDN);
-	mpfr_setsign(mpfr_op2, mpfr_op2, sign2, MPFR_RNDN);
-	// MPFR does not provide rounding to nearest tie to away at the moment :
-	mpfr_add(mpfr_res, mpfr_op1, mpfr_op2, MPFR_RNDN);
-	auto mpfr_exp = mpfr_get_exp(mpfr_res) - 1;
-	mpfr_mul_2si(mpfr_res, mpfr_res, targetWF-mpfr_exp, MPFR_RNDN);
-	uint64_t mpfr_res_signif = mpfr_get_ui(mpfr_res, MPFR_RNDN);
-	uint64_t mpfr_res_frac = mpfr_res_signif & ((uint64_t{1} << targetWF) - 1);
-	auto mpfr_res_sign = mpfr_signbit(mpfr_res);
-	mpfr_clears(mpfr_op1, mpfr_op2, mpfr_res, static_cast<mpfr_ptr>(0));
-	//----------------------------------------------------------------------------
 
-	BOOST_REQUIRE_MESSAGE(res.getFraction().unravel() == mpfr_res_frac, "mpfr and expression results fraction differs");
-	BOOST_REQUIRE_MESSAGE(res.getExponent().unravel() == mpfr_exp, "mpfr and expression result differs on the exponent");
-	BOOST_REQUIRE_MESSAGE(res.getSign().unravel() == mpfr_res_sign, "mpfr and expression results differs on the sign");
-	BOOST_REQUIRE_MESSAGE(res.isInf().unravel() == 0, "Result inf flag is set, that should not happen");
-	BOOST_REQUIRE_MESSAGE(res.isNaN().unravel() == 0, "Result NaN flag is set, that should not happen");
+	constexpr uint64_t signif_mask2 = {1 << Dim2::WF};
+	uint64_t signif2 = frac2 | signif_mask2;
+	mpfr_set_ui_2exp(mpfr_op2, signif2, exp2 - Dim2::WF, MPFR_RNDN);
+
+
+	mpfr_setsign(mpfr_op1, mpfr_op1, s1, MPFR_RNDN);
+	mpfr_setsign(mpfr_op2, mpfr_op2, s2, MPFR_RNDN);
+	// MPFR does not provide rounding to nearest tie to away at the moment :
+	// Work around : perform round down with one bit extra precision
+	mpfr_add(mpfr_sum, mpfr_op1, mpfr_op2, MPFR_RNDZ);
+	auto mpfr_exp = mpfr_get_exp(mpfr_sum) - 1;
+	mpfr_mul_2si(mpfr_round, mpfr_sum, targetWF - mpfr_exp, MPFR_RNDN);
+	mpfr_abs(mpfr_round, mpfr_round, MPFR_RNDN);
+	uint64_t mpfr_extra_prec_rd = mpfr_get_ui(mpfr_round, MPFR_RNDA);
+	if (mpfr_extra_prec_rd > ((uint64_t{1} << (targetWF + 1))  - 1)) {
+		++mpfr_exp;
+	}
+	bool mpfr_is_zero = (mpfr_extra_prec_rd == 0);
+	uint64_t mpfr_res_frac = mpfr_extra_prec_rd & ((uint64_t{1} << targetWF) - 1);
+	auto mpfr_res_sign = mpfr_signbit(mpfr_sum);
+
+	bool ok_zero = not(mpfr_is_zero xor res_is_zero);
+	BOOST_CHECK_MESSAGE(ok_zero, "Error in non-zero result in mpfr or expression");
+
+	bool fraction_ok = (res.getFraction().unravel() == mpfr_res_frac);
+	BOOST_CHECK_MESSAGE(fraction_ok, "mpfr and expression results fraction differs");
+	if (not fraction_ok) {
+		cerr << "Expected : " << mpfr_res_frac << endl;
+		cerr << "Got : " << res.getFraction().unravel() << endl;
+
+	}
+
+	bool exp_ok = (res.getExponent().unravel() == mpfr_exp) || res_is_zero;
+	BOOST_CHECK_MESSAGE(exp_ok, "mpfr and expression result differs on the exponent");
+
+	bool sign_ok = (res.getSign().unravel() == mpfr_res_sign) || res_is_zero;
+	BOOST_CHECK_MESSAGE(sign_ok, "mpfr and expression results differs on the sign");
+
+	bool isInfOk = (res.isInf().unravel() == 0);
+	BOOST_CHECK_MESSAGE(isInfOk, "Result inf flag is set, that should not happen");
+
+	bool isNaNOk = (res.isNaN().unravel() == 0);
+	BOOST_CHECK_MESSAGE(isNaNOk, "Result NaN flag is set, that should not happen");
+
+	auto res_bool = fraction_ok and exp_ok and sign_ok and isInfOk and isNaNOk and ok_zero;
+	if (!res_bool) {
+		mpfr_dump(mpfr_op1);
+		mpfr_dump(mpfr_op2);
+		mpfr_dump(mpfr_sum);
+		mpfr_dump(mpfr_round);
+	}
+	mpfr_clears(mpfr_op1, mpfr_op2, mpfr_sum, mpfr_round, static_cast<mpfr_ptr>(0));
+	return res_bool;
 }
 
 BOOST_AUTO_TEST_CASE(TestAdditionNoOverlap)
 {
 	using dim1 = TightFPDim<15, 18, -5>;
 	using dim2 = TightFPDim<8, 7, -26>;
-	check_addition_standard<dim1, dim2, 15>(15, -2, 0, 0);
+	check_addition_standard<dim1, dim2, 15>(0, 0, 15, -2, 0, 0);
+}
+
+BOOST_AUTO_TEST_CASE(TestAdditionSimpleOverlap)
+{
+	using dim1 = TightFPDim<15, 18, -5>;
+	using dim2 = TightFPDim<8, 7, -26>;
+	check_addition_standard<dim1, dim2, 15>(0, 0, 6, 4, 0, 0);
+}
+
+BOOST_AUTO_TEST_CASE(AdditionOpposite)
+{
+	constexpr vec_width WE = 5;
+	constexpr vec_width WF = 9;
+	constexpr vec_width targetWF = WF;
+	using dim = FPDim<WE, WF>;
+
+	bool res = check_addition_standard<dim, dim, targetWF>(
+				0, 1, -16, -16, 0, 0
+				);
+
+	BOOST_REQUIRE(res);
+}
+
+BOOST_AUTO_TEST_CASE(AdditionDoubleNeg)
+{
+	constexpr vec_width WE = 5;
+	constexpr vec_width WF = 9;
+	constexpr vec_width targetWF = WF;
+	using dim = FPDim<WE, WF>;
+
+	bool res = check_addition_standard<dim, dim, targetWF>(
+				1, 1, -16, -16, 0, 0
+				);
+
+	BOOST_REQUIRE(res);
+}
+
+BOOST_AUTO_TEST_CASE(AddOpSignOneBitCancel)
+{
+	constexpr vec_width WE = 5;
+	constexpr vec_width WF = 9;
+	constexpr vec_width targetWF = WF;
+	using dim = FPDim<WE, WF>;
+
+	bool res = check_addition_standard<dim, dim, targetWF>(
+				0, 1, -16, -14, 0, 0
+				);
+
+	BOOST_REQUIRE(res);
+}
+
+BOOST_AUTO_TEST_CASE(TestAdditionCompleteFormat)
+{
+	constexpr vec_width WE = 5;
+	constexpr vec_width WF = 7;
+	constexpr vec_width targetWF = WF;
+	using dim = FPDim<WE, WF>;
+	constexpr int64_t min_significand{0}, max_ex_significand{1 << WF};
+
+	/**** Using expr ********/
+
+	using fpnum = FPNumber<dim, VivadoWrapper>;
+
+
+	/**** Using mpfr ****/
+	constexpr uint64_t signif_mask{1 << WF};
+	mpfr_t mpfr_op1, mpfr_op2, mpfr_sum, mpfr_round;
+	mpfr_init2(mpfr_op1, WF + 1);
+	mpfr_init2(mpfr_op2, WF + 1);
+	mpfr_init2(mpfr_sum, targetWF + 2);
+	mpfr_init2(mpfr_round, targetWF + 2);
+	for (int64_t exp1 = dim::MIN_EXP ; exp1 <= dim::MAX_EXP ; ++exp1) {
+		for (int64_t frac1 = min_significand; frac1 < max_ex_significand ; ++frac1) {
+			uint64_t signif1 = frac1 | signif_mask;
+			mpfr_set_ui_2exp(mpfr_op1, signif1, exp1 - WF, MPFR_RNDN);
+			for (int64_t exp2 = exp1 ; exp2 <= dim::MAX_EXP ; ++exp2) {
+				for (int64_t frac2 = frac1; frac2 < max_ex_significand ; ++frac2) {
+					uint64_t signif2 = frac2 | signif_mask;
+					mpfr_set_ui_2exp(mpfr_op2, signif2, exp2 - WF, MPFR_RNDN);
+					for (int64_t s1 = 0 ; s1 <= 1 ; s1++) {
+						for(int64_t s2 = 0 ; s2 <=1 ; s2++) {
+							/*** Expressions ***/
+							fpnum op2{{{frac2}}, {{exp2}}, {{s2}}, {{0}}, {{0}}, {{0}}};
+							fpnum op1{{{frac1}}, {{exp1}}, {{s1}}, {{0}}, {{0}}, {{0}}};
+
+							auto eop1 = to_expr(op1);
+							auto eop2 = to_expr(op2);
+
+							auto exprres = eop1+eop2;
+							auto res = exprres.template computeWithTargetPrecision<targetWF>();
+							bool res_is_zero = res.isZero().unravel();
+
+							/*** MPFR ***/
+							mpfr_setsign(mpfr_op1, mpfr_op1, s1, MPFR_RNDN);
+							mpfr_setsign(mpfr_op2, mpfr_op2, s2, MPFR_RNDN);
+							// MPFR does not provide rounding to nearest tie to away at the moment :
+							// Work around : perform round down with one bit extra precision
+							mpfr_add(mpfr_sum, mpfr_op1, mpfr_op2, MPFR_RNDZ);
+							auto mpfr_exp = mpfr_get_exp(mpfr_sum) - 1;
+							mpfr_mul_2si(mpfr_round, mpfr_sum, targetWF - mpfr_exp, MPFR_RNDN);
+							mpfr_abs(mpfr_round, mpfr_round, MPFR_RNDN);
+							uint64_t mpfr_extra_prec_rd = mpfr_get_ui(mpfr_round, MPFR_RNDA);
+							if (mpfr_extra_prec_rd > ((uint64_t{1} << (targetWF + 1))  - 1)) {
+								++mpfr_exp;
+							}
+							bool mpfr_is_zero = (mpfr_extra_prec_rd == 0);
+							uint64_t mpfr_res_frac = mpfr_extra_prec_rd & ((uint64_t{1} << targetWF) - 1);
+							auto mpfr_res_sign = mpfr_signbit(mpfr_sum);
+
+							bool ok_zero = not(mpfr_is_zero xor res_is_zero);
+							BOOST_CHECK_MESSAGE(ok_zero, "Error in non-zero result in mpfr or expression");
+
+							bool fraction_ok = (res.getFraction().unravel() == mpfr_res_frac);
+							BOOST_CHECK_MESSAGE(fraction_ok, "mpfr and expression results fraction differs");
+							if (not fraction_ok) {
+								cerr << "Expected : " << mpfr_res_frac << endl;
+								cerr << "Got : " << res.getFraction().unravel() << endl;
+
+							}
+
+							bool exp_ok = (res.getExponent().unravel() == mpfr_exp) || res_is_zero;
+							BOOST_CHECK_MESSAGE(exp_ok, "mpfr and expression result differs on the exponent");
+
+							bool sign_ok = (res.getSign().unravel() == mpfr_res_sign) || res_is_zero;
+							BOOST_CHECK_MESSAGE(sign_ok, "mpfr and expression results differs on the sign");
+
+							bool isInfOk = (res.isInf().unravel() == 0);
+							BOOST_CHECK_MESSAGE(isInfOk, "Result inf flag is set, that should not happen");
+
+							bool isNaNOk = (res.isNaN().unravel() == 0);
+							BOOST_CHECK_MESSAGE(isNaNOk, "Result NaN flag is set, that should not happen");
+
+							auto res_bool = fraction_ok and exp_ok and sign_ok and isInfOk and isNaNOk and ok_zero;
+
+							if (not res_bool) {
+								cerr << "Error with <" << std::abs(s1) << ", " << exp1 << ", " << frac1 << "> + <" << std::abs(s2) <<
+										", " << exp2 << ", " << frac2 << ">" << endl;
+								mpfr_dump(mpfr_op1);
+								mpfr_dump(mpfr_op2);
+								mpfr_dump(mpfr_sum);
+								mpfr_dump(mpfr_round);
+							}
+							BOOST_REQUIRE(res_bool);
+						}
+					}
+				}
+			}
+		}
+	}
+	mpfr_clears(mpfr_op1, mpfr_op2, mpfr_sum, mpfr_round, static_cast<mpfr_ptr>(0));
+}
+
+BOOST_AUTO_TEST_CASE(TestAdditionCompleteInequalFormats)
+{
+	constexpr vec_width WE1 = 5;
+	constexpr vec_width WF1 = 7;
+
+	constexpr vec_width WE2 = 6;
+	constexpr vec_width WF2 = 5;
+
+	constexpr vec_width targetWF = WF1+2;
+
+	using dim1 = FPDim<WE1, WF1>;
+	using dim2 = FPDim<WE2, WF2>;
+	constexpr int64_t min_significand{0}, max_ex_significand1{1 << WF1}, max_ex_significand2{1 << WF2};
+
+	/**** Using expr ********/
+
+	using fpnum1 = FPNumber<dim1, VivadoWrapper>;
+	using fpnum2 = FPNumber<dim2, VivadoWrapper>;
+
+
+	/**** Using mpfr ****/
+	mpfr_t mpfr_op1, mpfr_op2, mpfr_sum, mpfr_round;
+	mpfr_init2(mpfr_op1, WF1 + 1);
+	mpfr_init2(mpfr_op2, WF2 + 1);
+	mpfr_init2(mpfr_sum, targetWF + 2);
+	mpfr_init2(mpfr_round, targetWF + 2);
+	for (int64_t exp1 = dim1::MIN_EXP ; exp1 <= dim1::MAX_EXP ; ++exp1) {
+		for (int64_t frac1 = min_significand; frac1 < max_ex_significand1 ; ++frac1) {
+			uint64_t signif1 = frac1 | max_ex_significand1;
+			mpfr_set_ui_2exp(mpfr_op1, signif1, exp1 - WF1, MPFR_RNDN);
+			for (int64_t exp2 = dim2::MIN_EXP ; exp2 <= dim2::MAX_EXP ; ++exp2) {
+				for (int64_t frac2 = min_significand; frac2 < max_ex_significand2 ; ++frac2) {
+					uint64_t signif2 = frac2 | max_ex_significand2;
+					mpfr_set_ui_2exp(mpfr_op2, signif2, exp2 - WF2, MPFR_RNDN);
+					for (int64_t s1 = 0 ; s1 <= 1 ; s1++) {
+						for(int64_t s2 = 0 ; s2 <=1 ; s2++) {
+							/*** Expressions ***/
+							fpnum2 op2{{{frac2}}, {{exp2}}, {{s2}}, {{0}}, {{0}}, {{0}}};
+							fpnum1 op1{{{frac1}}, {{exp1}}, {{s1}}, {{0}}, {{0}}, {{0}}};
+
+							auto eop1 = to_expr(op1);
+							auto eop2 = to_expr(op2);
+
+							auto exprres = eop1+eop2;
+							auto res = exprres.template computeWithTargetPrecision<targetWF>();
+							bool res_is_zero = res.isZero().unravel();
+
+							/*** MPFR ***/
+							mpfr_setsign(mpfr_op1, mpfr_op1, s1, MPFR_RNDN);
+							mpfr_setsign(mpfr_op2, mpfr_op2, s2, MPFR_RNDN);
+							// MPFR does not provide rounding to nearest tie to away at the moment :
+							// Work around : perform round down with one bit extra precision
+							mpfr_add(mpfr_sum, mpfr_op1, mpfr_op2, MPFR_RNDZ);
+							auto mpfr_exp = mpfr_get_exp(mpfr_sum) - 1;
+							mpfr_mul_2si(mpfr_round, mpfr_sum, targetWF - mpfr_exp, MPFR_RNDN);
+							mpfr_abs(mpfr_round, mpfr_round, MPFR_RNDN);
+							uint64_t mpfr_extra_prec_rd = mpfr_get_ui(mpfr_round, MPFR_RNDA);
+							if (mpfr_extra_prec_rd > ((uint64_t{1} << (targetWF + 1))  - 1)) {
+								++mpfr_exp;
+							}
+							bool mpfr_is_zero = (mpfr_extra_prec_rd == 0);
+							uint64_t mpfr_res_frac = mpfr_extra_prec_rd & ((uint64_t{1} << targetWF) - 1);
+							auto mpfr_res_sign = mpfr_signbit(mpfr_sum);
+
+							bool ok_zero = not(mpfr_is_zero xor res_is_zero);
+							BOOST_CHECK_MESSAGE(ok_zero, "Error in non-zero result in mpfr or expression");
+
+							bool fraction_ok = (res.getFraction().unravel() == mpfr_res_frac);
+							BOOST_CHECK_MESSAGE(fraction_ok, "mpfr and expression results fraction differs");
+							if (not fraction_ok) {
+								cerr << "Expected : " << mpfr_res_frac << endl;
+								cerr << "Got : " << res.getFraction().unravel() << endl;
+
+							}
+
+							bool exp_ok = (res.getExponent().unravel() == mpfr_exp) || res_is_zero;
+							BOOST_CHECK_MESSAGE(exp_ok, "mpfr and expression result differs on the exponent");
+
+							bool sign_ok = (res.getSign().unravel() == mpfr_res_sign) || res_is_zero;
+							BOOST_CHECK_MESSAGE(sign_ok, "mpfr and expression results differs on the sign");
+
+							bool isInfOk = (res.isInf().unravel() == 0);
+							BOOST_CHECK_MESSAGE(isInfOk, "Result inf flag is set, that should not happen");
+
+							bool isNaNOk = (res.isNaN().unravel() == 0);
+							BOOST_CHECK_MESSAGE(isNaNOk, "Result NaN flag is set, that should not happen");
+
+							auto res_bool = fraction_ok and exp_ok and sign_ok and isInfOk and isNaNOk and ok_zero;
+
+							if (not res_bool) {
+								cerr << "Error with <" << std::abs(s1) << ", " << exp1 << ", " << frac1 << "> + <" << std::abs(s2) <<
+										", " << exp2 << ", " << frac2 << ">" << endl;
+								mpfr_dump(mpfr_op1);
+								mpfr_dump(mpfr_op2);
+								mpfr_dump(mpfr_sum);
+								mpfr_dump(mpfr_round);
+							}
+							BOOST_REQUIRE(res_bool);
+						}
+					}
+				}
+			}
+		}
+	}
+	mpfr_clears(mpfr_op1, mpfr_op2, mpfr_sum, mpfr_round, static_cast<mpfr_ptr>(0));
+}
+
+#else
+BOOST_AUTO_TEST_CASE(WarningIncompleteTests)
+{
+	std::cerr << "Warning : the test executable was not linked against mpfr, so test on SUM and Round operator will not be performed" << std::endl;
+	BOOST_WARN_MESSAGE(true, "The current test executable was compiled without mpfr support, Sum and rounding component will not be tested.");
 }
 #endif
