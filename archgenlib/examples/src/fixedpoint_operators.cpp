d0657c3313df719fe7fd90a849de730837cc6e91
Luc Forget <luc.forget@insa-lyon.fr>
2022-03-25 18:15:05 -0700

    Adding forgotten test file

---
+++ archgenlib/examples/src/fixedpoint_operators.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+
+#include "bitint_tools/type_helpers.hpp"
+#include "fixedpoint/fixedpoint.hpp"
+
+using namespace archgenlib;
+
+int main() {
+  FixedFormat<4, -2, false> format{};
+  hint::detail::bitint_base_t<false, 7> val{0b01010};
+  FixedNumber op1(format, {0b01010});
+  FixedNumber<FixedFormat<3, -5, false>>op2({0b11111});
+
+  //      01010
+  // +       11111
+  // ---------------
+  //     001101111
+
+  FixedNumber<FixedFormat<5, -5, false>> res({0b1101111});
+  assert(res == (op1 + op2));
+  return 0;
+}
