9904d1b25889887e36fa8cc14ee92027d8e637d0
Tyker <tyker1@outlook.com>
2022-08-19 17:59:26 -0700

    For use polynom calculation done in double instead of fixedpoint because they are wrong.

--- archgenlib/examples/src/test_expr.cpp
+++ archgenlib/examples/src/test_expr.cpp
@@ -12,6 +12,9 @@
 #include "fixedpoint/evaluate.hpp"
 #include "fixedpoint/fixedpoint.hpp"
 
+/// debug function provided by archgen MLIR for now
+extern "C" double polynom_double_0(double);
+
 template <typename T> static constexpr bool ok = false;
 
 template <archgenlib::ExpressionType T> static constexpr bool ok<T> = true;
@@ -20,7 +23,7 @@ using archgenlib::bitweight_t;
 
 auto compute_ref(auto val) {
   auto val_d = val.template get_as<double>();
-  auto sin = std::sin(val_d * M_PI);
+  auto sin = std::sin(val_d /2 * M_PI);
   return sin;
 }
 
@@ -30,8 +33,9 @@ bool check_against_ref(auto val, auto res) {
 
   auto x = val.template get_as<double>();
   auto outprec = decltype(res)::format_t::lsb_weight;
-  auto resval = res.template get_as<double>();
+  // auto resval = res.template get_as<double>();
   auto ref = compute_ref(val);
+  auto resval = polynom_double_0(x);
   auto diffabs = std::abs(ref - resval);
   static const double err_budget = ldexp(double{1}, outprec);
   if (diffabs < err_budget)
@@ -48,45 +52,17 @@ using fpnum_t = archgenlib::FixedNumber<fpdim_t>;
 
 using storage_t =
     hint::detail::bitint_base_t<fpdim_t::is_signed, fpdim_t::width>;
-
-constexpr bitweight_t outprec = -10;
-
-inline auto convert_to_double(storage_t val) {
-  auto val_d = static_cast<double>(val);
-  val_d = ldexp(val_d, fpdim_t::lsb_weight);
-  return val_d;
-}
-
-auto compute_ref(storage_t val) {
-  auto val_d = convert_to_double(val);
-  auto sin = std::sin(val_d / 64 * std::numbers::pi);
-  return sin;
-}
-
-bool compare_ref(storage_t val, auto res) {
-  auto res_int = res.value();
-  if (res_int > (1 << (res.width - 1))) {
-    res_int -= (1 << res.width);
-  }
-  auto resval = static_cast<double>(res_int);
-  resval = ldexp(resval, outprec);
-  auto ref = compute_ref(val);
-  auto diffabs = std::abs(ref - resval);
-  static const double err_budget = ldexp(double{1}, outprec);
-  return diffabs < err_budget;
-}
-
+ 
 int main() {
   using storage_t = unsigned _BitInt(fpdim_t::width);
   for (unsigned int i = 0; i < (1 << fpdim_t::width); ++i) {
     auto val = static_cast<storage_t>(i);
     fpnum_t val_fixed{val};
     auto a = archgenlib::FreeVariable(val_fixed);
-    auto s = a * 0x1p-7_cst * archgenlib::pi;
-    auto c = archgenlib::sin(s);
-    auto res = archgenlib::evaluate<archgenlib::FixedFormat<1, -10, signed>>(c);
-    if constexpr (archgenlib::has_specialization_header) {
-      assert(compare_ref(val, res));
+    auto c = archgenlib::sin(a * archgenlib::pi / 0x2p0_cst);
+    auto res = archgenlib::evaluate<archgenlib::FixedFormat<-1, -3, unsigned>>(c);
+    if constexpr (archgenlib::has_implementation) {
+      check_against_ref(val_fixed, res); 
     }
   }
   if constexpr (archgenlib::has_implementation) { 
